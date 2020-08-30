d51b527fb6d2d00706884ca0c01389c0763509ef
chengqiang.liu <liuchengqiang007@163.com>
2020-08-30 10:14:31 +0900

    init a blank react app

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import {Button} from 'antd';
+import 'antd/dist/antd.less';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Button type="primary">
+                Primary
+            </Button>
+        );
+    }
+}
